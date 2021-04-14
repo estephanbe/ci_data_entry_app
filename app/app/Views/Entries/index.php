fe8dc16ef1cc6953ddfc871203ebc5d465e640fa
Bisharah <estephan.be@gmail.com>
2021-04-14 23:26:16 +0300

    responsiveness

--- app/app/Views/Entries/index.php
+++ app/app/Views/Entries/index.php
@@ -1,47 +1,47 @@
 <div class="me-5 ms-5" id="uae-action-main-header">
     <div class="row align-items-center">
-        <?= form_open('/', array('class' => 'd-flex flex-wrap col-12 col-sm-8', 'method' => 'GET')) ?>
-            <button class="btn btn-outline-success" type="submit">إبحث</button>
-            <div class="d-flex">
-                <span class="uae-search-seperator">في</span>
-                <select name="search_filter" id="aue-search-type" class="me-2 form-select w-75">
-                    <option value="name">الأسماء</option>
-                    <option value="country">البلدان</option>
-                    <option value="natonality">الجنسيات</option>
-                    <option value="occupation">المِهن</option>
-                </select>
-                <span class="uae-search-seperator">عن</span>
-            </div>
-            <div class="d-flex">
-                <input class="form-control me-0 me-md-2 w-100 mt-3 mt-md-0" name="search_term" type="search" placeholder="كلمة البحث" aria-label="Search">
-            </div>
+        <?= form_open('/', array('class' => 'd-flex flex-wrap align-items-stretch col-12 col-lg-8', 'method' => 'GET')) ?>
+        <button class="btn btn-outline-success" type="submit">إبحث</button>
+        <span class="uae-search-seperator">في</span>
+        <div class="d-flex">
+            <select name="search_filter" id="aue-search-type" class="me-2 form-select w-75">
+                <option value="name">الأسماء</option>
+                <option value="country">البلدان</option>
+                <option value="natonality">الجنسيات</option>
+                <option value="occupation">المِهن</option>
+            </select>
+            <span class="uae-search-seperator">عن</span>
+        </div>
+        <div id="uae-main-search-wrapper" class="d-flex w-100 mt-3 mt-md-0 me-md-2">
+            <input class="form-control w-100" name="search_term" type="search" placeholder="كلمة البحث" aria-label="Search">
+        </div>
         </form>
-        <div id="group-action-btns-wrapper" class="col-12 col-sm-4 mt-3 mt-sm-0">
-            <div id="group-action-btns" class="d-flex justify-content-end">
-                <?php if (! $is_search ): ?>
-                    <select name="search-type" id="aue-num-of-entries" class="ms-2 form-select" title="عدد المتعاونين في كل صفحة">
-                        <option value="10">10</option>
-                        <option value="20">20</option>
-                        <option value="50">50</option>
-                        <option value="all">الكل</option>
-                    </select>
-                <?php else: ?>
-                    <a href="<?= base_url(); ?>" class="btn btn-secondary ms-2">
-                        عودة إلى القائمة الرئيسية
-                    </a>
-                <?php endif; ?>
+        <div id="group-action-btns-wrapper" class="col-12 col-lg-4 mt-3 mt-lg-0">
+            <div id="group-action-btns" class="d-flex justify-content-start justify-content-lg-end flex-wrap">
                 <button type="button" id="uae-group-action-print" class="uae-action-btn btn btn-secondary">
                     طباعة
                 </button>
                 <a href="#" id="uae-group-action-excel" class="uae-action-btn btn btn-secondary me-2">
                     Excel
                 </a>
+                <?php if (!$is_search) : ?>
+                    <select name="search-type" id="aue-num-of-entries" class="form-select me-2" title="عدد المتعاونين في كل صفحة">
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                        <option value="50">50</option>
+                        <option value="all">الكل</option>
+                    </select>
+                <?php else : ?>
+                    <a href="<?= base_url(); ?>" class="btn btn-secondary me-2">
+                        عودة إلى القائمة الرئيسية
+                    </a>
+                <?php endif; ?>
             </div>
         </div>
     </div>
     <div class="row mt-2">
-        <div class="col-12 d-flex justify-content-end align-items-center">
-            عدد جميع المتعاونين: 
+        <div class="col-12 d-flex justify-content-start justify-content-lg-end align-items-center">
+            عدد جميع المتعاونين:
             <strong class="me-2">
                 <?= $entries_count ?>
             </strong>
@@ -49,7 +49,7 @@
     </div>
 </div>
 
-<div id="aue-main-table" class="m-5 mt-3">
+<div id="aue-main-table" class="m-5 mt-3 table-responsive">
     <table class="table table-hover">
         <thead>
             <tr>
@@ -78,12 +78,12 @@
                             <i class="bi bi-eye"></i>
                         </a>
                         <?php if (session()->get('is_admin')) : ?>
-                        <a href="<?= base_url('entries/' . $value['id'] . '/edit'); ?>" class="uae-action-btn badge rounded-pill bg-warning">
-                            <i class="bi bi-pencil-fill"></i>
-                        </a>
-                        <a href="#" onclick="deleteEntryTable(<?= $value['id'] ?>, '<?= $value['name'] ?>')" class="uae-action-btn uae-action-btn-delete badge rounded-pill bg-danger">
-                            <i class="bi bi-trash"></i>
-                        </a>
+                            <a href="<?= base_url('entries/' . $value['id'] . '/edit'); ?>" class="uae-action-btn badge rounded-pill bg-warning">
+                                <i class="bi bi-pencil-fill"></i>
+                            </a>
+                            <a href="#" onclick="deleteEntryTable(<?= $value['id'] ?>, '<?= $value['name'] ?>')" class="uae-action-btn uae-action-btn-delete badge rounded-pill bg-danger">
+                                <i class="bi bi-trash"></i>
+                            </a>
                         <?php endif; ?>
                     </td>
                 </tr>
@@ -92,16 +92,23 @@
     </table>
 </div>
 
-<?php if (! $is_search ): ?>
-<div class="row m-5" id="uae-main-page-pages-nav">
-    <div class="col-4 d-flex justify-content-start align-items-center">
-        <button class="btn btn-primary btn-lg" id="uae-prev-btn">الصفحة السابقة</button>
+<?php if (!$is_search) : ?>
+    <div class="row m-5" id="uae-main-page-pages-nav">
+        <div class="col-6 col-lg-4 d-flex justify-content-start align-items-center order-1 order-lg-0">
+            <button class="btn btn-primary btn-lg" id="uae-prev-btn">الصفحة السابقة</button>
+        </div>
+        <div id="uae-page-number-wrapper" class="col-12 col-lg-4 d-flex flex-wrap justify-content-center align-items-center order-0 order-lg-1 mb-3 mb-lg-0">
+            <div>
+                صفحة رقم
+                <span id="uae-page-number" class="me-2 ms-2"></span>
+            </div>
+            <div>
+                من أصل
+                <span class="me-2 ms-2" id="uae-page-total-number"></span>
+            </div>
+        </div>
+        <div class="col-6 col-lg-4 d-flex justify-content-end align-items-center order-2 order-lg-2">
+            <button class="btn btn-primary btn-lg" id="uae-next-btn">الصفحة التالية</button>
+        </div>
     </div>
-    <div id="uae-page-number-wrapper" class="col-4 d-flex justify-content-center align-items-center">
-                صفحة رقم <span id="uae-page-number" class="me-2 ms-2"></span> من أصل <span class="me-2 ms-2" id="uae-page-total-number"></span>
-    </div>
-    <div class="col-4 d-flex justify-content-end align-items-center">
-        <button class="btn btn-primary btn-lg" id="uae-next-btn">الصفحة التالية</button>
-    </div>
-</div>
 <?php endif; ?>
--- app/app/Views/users.php
+++ app/app/Views/users.php
@@ -4,32 +4,34 @@
             <?= session()->getFlashdata('success'); ?>
         </div>
     <?php endif; ?>
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th scope="col">إسم المستخدم</th>
-                <th scope="col">الإسم الظاهر</th>
-                <th scope="col">الدور</th>
-                <th scope="col" class="uae-single-action-header">
-                    <a href="<?= base_url() . '/users/new' ?>" class="btn btn-success btn-sm">
-                        إضافة مستخدم
-                    </a>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($users as $value) : ?>
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead>
                 <tr>
-                    <td><?= $value['username']; ?></td>
-                    <td><?= $value['display_name']; ?></td>
-                    <td><?= $value['is_admin'] ? "مدير" : "مشاهد"; ?></td>
-                    <td class="uae-single-action">
-                        <a href="<?= base_url() . '/users/edit/?user_id=' . $value['id'] ?>" class="uae-user-action-btn btn btn-warning btn-sm">
-                            تعديل البيانات
+                    <th scope="col">إسم المستخدم</th>
+                    <th scope="col">الإسم الظاهر</th>
+                    <th scope="col">الدور</th>
+                    <th scope="col" class="uae-single-action-header">
+                        <a href="<?= base_url() . '/users/new' ?>" class="btn btn-success btn-sm">
+                            إضافة مستخدم
                         </a>
-                    </td>
+                    </th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($users as $value) : ?>
+                    <tr>
+                        <td><?= $value['username']; ?></td>
+                        <td><?= $value['display_name']; ?></td>
+                        <td><?= $value['is_admin'] ? "مدير" : "مشاهد"; ?></td>
+                        <td class="uae-single-action">
+                            <a href="<?= base_url() . '/users/edit/?user_id=' . $value['id'] ?>" class="uae-user-action-btn btn btn-warning btn-sm">
+                                تعديل البيانات
+                            </a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
